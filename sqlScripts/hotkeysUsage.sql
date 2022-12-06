5e1345add3854f1d2b7ffd03bb40e5a58fcad9ac
bis <bis@skbkontur.ru>
2022-12-06 11:17:39 +0500

    Выводим по частотности

--- sqlScripts/hotkeysUsage.sql
+++ sqlScripts/hotkeysUsage.sql
@@ -1,4 +1,5 @@
-select visitParamExtractString(Value, 'data'), count(*)
+select visitParamExtractString(Value, 'data') HotkeyName, count(*) Count
 from productivity.stats
 where ActionType = 'Action'
 group by visitParamExtractString(Value, 'data')
+order by Count desc
