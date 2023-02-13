05a1c7cba7928e5b36f5fa72d928d174c8b89de2
bis <bis@skbkontur.ru>
2023-02-13 11:35:18 +0530

    метрики навигации

--- sqlScripts/navigationStats.sql
+++ sqlScripts/navigationStats.sql
@@ -1,10 +1,10 @@
-select sessions.SessionId           as SessionId,
-       sessions.username            as Login,
-       hotkeys_2.navigationsCount   as NavigationsCount,
-       mouseClicks.mouseClicksCount as MouseClicks,
-       mouseWheels.mouseWheelCount  as MouseWheels,
-       mouseMoves.distance          as MouseDistance,
-       hotkeys_2.navigationsCount+mouseClicks.mouseClicksCount+mouseWheels.mouseWheelCount +mouseMoves.distance as Total
+select sessions.SessionId                                                                      as SessionId,
+       sessions.username                                                                       as Login,
+       hotkeys_2.navigationsCount + mouseClicks.mouseClicksCount + mouseWheels.mouseWheelCount as Total,
+       hotkeys_2.navigationsCount                                                              as NavigationsCount,
+       mouseClicks.mouseClicksCount                                                            as MouseClicks,
+       mouseWheels.mouseWheelCount                                                             as MouseWheels
+
 from
     -- Сессии
     (select SessionId,
@@ -54,42 +54,4 @@ from
        and splitByChar(':', visitParamExtractString(Value, 'data'))[1] = 'wheel'
      group by SessionId) mouseWheels
     on sessions.SessionId = mouseWheels.SessionId
-
-        -- пробег мышки
-        left join
-    (select SessionId,
-            sum(L2Distance((toFloat32(right.x), toFloat32(right.y)),
-                           (toFloat32(left.x), toFloat32(left.y)))) distance
-     from (
-              select x, y, number, SessionId
-              from (
-                       select splitByChar(':', visitParamExtractString(Value, 'data'))[2] x,
-                              splitByChar(':', visitParamExtractString(Value, 'data'))[3] y,
-                              Timestamp,
-                              rowNumberInBlock()                                          number,
-                              SessionId
-                       from productivity.stats
-                       where ActionType = 'MouseEvent'
-                         and splitByChar(':', visitParamExtractString(Value, 'data'))[1] = 'move'
-                       )
-              order by Timestamp, number
-              ) as left
-              inner join
-          (
-              select x, y, number, SessionId
-              from (
-                       select splitByChar(':', visitParamExtractString(Value, 'data'))[2] x,
-                              splitByChar(':', visitParamExtractString(Value, 'data'))[3] y,
-                              Timestamp,
-                              rowNumberInBlock()                                          number,
-                              SessionId
-                       from productivity.stats
-                       where ActionType = 'MouseEvent'
-                         and splitByChar(':', visitParamExtractString(Value, 'data'))[1] = 'move'
-                       )
-              order by Timestamp, number
-              ) as right
-          on left.SessionId = right.SessionId and left.number = right.number + 1
-     group by SessionId) mouseMoves
-    on mouseMoves.SessionId = sessions.SessionId
 order by sessions.start desc;
